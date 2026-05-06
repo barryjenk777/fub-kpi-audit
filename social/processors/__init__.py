2ccd9220f94332b3d24732af300fb99df3b8c45f
Barry Jenkins <barry@yourfriendlyagent.net>
2026-05-06 06:57:12 -0400

    Revert "Merge pull request #7 from barryjenk777/claude/pensive-sammet-4e9a67"
    This reverts commit df033cea2a4ab608b2f7b5f2d8bc383bb138999d, reversing
    changes made to 2afcb2a7a270b5bb0fb6a7edc8cbeb2b634cb267.

--- social/processors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Per-input-type processors.
-
-Each processor implements:
-    classify(file_meta) -> bool  # does this processor handle this file?
-    treatment(file_meta) -> dict # what platforms + formats does it produce?
-"""
-from . import photo, link, note, audio, video  # noqa: F401
